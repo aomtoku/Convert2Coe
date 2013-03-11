4a8daecfd612589815a3bbeba62f0d6559831eb9
aomtoku <tokutoku1938@yahoo.co.jp>
2013-03-11 16:36:05 +0900

    first

---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+#include<ftcl.h>
+
+int main(int argc, char* argv)
+{
+    if(argc < 1){
+	printf("ERROR: USAGE: COMMAND <input file> <output file(*.coe)>");
+	exit(1);
+    }
+
+    FILE fi,fo;
+    fi = fopen(argv[1],"r");
+    fo = fopen(argv[2],"w");
+
+    while()
+
+
+    open("");
+    int = 0;
+    for(i=0;i<19200;i++){
+	write("");
+    }
+
+    return 0;
+}
